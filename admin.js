98317fda1cbaa946b3fa997b6f3ada4bea48d0cd
Brokenartyy <hasnakhaleedamumtaza@gmail.com>
2026-02-08 07:50:34 +0700

    udate init admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxzqdHlEFi5lIuen7vW9u2cxNbe3mPiio",
+  authDomain: "pony-ata.firebaseapp.com",
+  projectId: "pony-ata"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
 import {
   getAuth,
   onAuthStateChanged,
